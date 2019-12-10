85852aa3e4258505afe9ffe9b7dfb18bfa2bf148
unknown <Student@laptop-041.lib.iastate.edu>
2019-12-10 14:26:42 -0600

    Add pole to middle of thing

--- marchingCubesExample.js
+++ marchingCubesExample.js
@@ -33,19 +33,17 @@ effect.enableUvs = false;
 effect.enableColors = false;
 scene.add(effect);
 
-var clock = new THREE.Clock();
 var effectController;
-var time = 0;
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 effectController = {
     material: "shiny",
     speed: 1.0,
-    numBlobs: 10,
-    resolution: 28,
+    numBlobs: 0,
+    resolution: 24,
     isolation: 80,
-    floor: true,
+    floor: false,
     wallx: true,
     wallz: true,
     hue: 0.0,
@@ -61,8 +59,6 @@ effectController = {
 };
 
 var render = () => {
-    var delta = clock.getDelta();
-	time += delta * effectController.speed * 0.5;
 	// marching cubes
 	if ( effectController.resolution !== resolution ) {
 		resolution = effectController.resolution;
@@ -71,7 +67,7 @@ var render = () => {
 	if ( effectController.isolation !== effect.isolation ) {
 		effect.isolation = effectController.isolation;
     }
-    updateCubes( effect, time, effectController.numBlobs, effectController.floor, effectController.wallx, effectController.wallz );
+    updateCubes( effect, effectController.floor, effectController.wallx, effectController.wallz );
     renderer.render(scene,camera);
 };
 
@@ -81,27 +77,15 @@ var loop = () => {
     render();
 };
 
-function updateCubes( object, time, numblobs, floor, wallx, wallz ) {
+function updateCubes( object,floor, wallx, wallz ) {
     object.reset();
-    // fill the field with some metaballs
-    var i, ballx, bally, ballz, subtract, strength;
-    var rainbow = [
-        new THREE.Color( 0xff0000 ),
-        new THREE.Color( 0xff7f00 ),
-        new THREE.Color( 0xffff00 ),
-        new THREE.Color( 0x00ff00 ),
-        new THREE.Color( 0x0000ff ),
-        new THREE.Color( 0x4b0082 ),
-        new THREE.Color( 0x9400d3 )
-    ];
-    subtract = 12;
-    strength = 1.2 / ( ( Math.sqrt( numblobs ) - 1 ) / 4 + 1 );
-    for ( i = 0; i < numblobs; i ++ ) {
-        ballx = Math.sin( i + 1.26 * time * ( 1.03 + 0.5 * Math.cos( 0.21 * i ) ) ) * 0.27 + 0.5;
-        bally = Math.abs( Math.cos( i + 1.12 * time * Math.cos( 1.22 + 0.1424 * i ) ) ) * 0.77; // dip into the floor
-        ballz = Math.cos( i + 1.32 * time * 0.1 * Math.sin( ( 0.92 + 0.53 * i ) ) ) * 0.27 + 0.5;
-        object.addBall( ballx, bally, ballz, strength, subtract );
-        
+    for(x = 12; x < 16;x++) {
+        for(y = 12; y < 16; y++) {
+            for(z = 1; z< 24; z++) {
+                //console.log("Setting cell at " + x + " " + y + " " + z);
+                object.setCell(x,y,z,1500000000000.0);
+            }
+        }
     }
     if ( floor ) object.addPlaneY( 2, 12 );
     if ( wallz ) object.addPlaneZ( 2, 12 );
